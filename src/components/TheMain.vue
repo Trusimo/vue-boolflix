d652338a9dfe9a4e6c9bf82e632b9a9d1d012b13
Trusimo <simonetrunfio96@gmail.com>
2022-06-29 16:41:23 +0200

    console.log for fetchData

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -28,16 +28,16 @@ export default {
     methods: {
         fetchData() {
             console.log("topolino")
-            axios.get("https://api.themoviedb.org/3/search/", {
-                params: {
-                    api_key: "4748e32efaeb377f469c7ccfd87624c6",
-                    query: this.searchText,
-                    language: "it-IT",
-                },
-            })
-                .then((resp) => {
-                this.moviesList = resp.data.results;
-            });
+            // axios.get("https://api.themoviedb.org/3/search/", {
+            //     params: {
+            //         api_key: "4748e32efaeb377f469c7ccfd87624c6",
+            //         query: this.searchText,
+            //         language: "it-IT",
+            //     },
+            // })
+            //     .then((resp) => {
+            //     this.moviesList = resp.data.results;
+            // });
         },
     },
     mounted() {
